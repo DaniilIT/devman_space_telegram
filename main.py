802f5beec129e3fbe318fc80433fa05cdcb74022
Daniil_Manuilov <manuilovdaniil@yandex.ru>
2023-01-24 20:39:05 +0100

    add upload images using API SpaceX

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from pathlib import Path
 import requests
 
 
+SPACEX_URL = "https://api.spacexdata.com"
+FLIGHT_ID = '5eb87d47ffd86e000604b38a'
+
+
 def download_image(url, file_path):
     """Функция скачивает изображение и сохраняет его
     """
@@ -14,15 +18,20 @@ def download_image(url, file_path):
 
 
 def main():
-    image_url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-    image_file_path = "./images/hubble.jpeg"
-
     Path("./images").mkdir(exist_ok=True)
 
     try:
-        download_image(image_url, image_file_path)
+        response = requests.get(f'{SPACEX_URL}/v5/launches/{FLIGHT_ID}')
+        response.raise_for_status()
     except requests.exceptions.HTTPError:
         print("Что-то пошло не так")
+    else:
+        for image_number, image_url in enumerate(response.json()['links']['flickr']['original']):
+            try:
+                image_file_path = f"./images/spacex{image_number}.jpg"
+                download_image(image_url, image_file_path)
+            except requests.exceptions.HTTPError:
+                print("Что-то пошло не так")
 
 
 if __name__ == '__main__':
