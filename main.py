51265da7ab191bceeb20743f4be01d36ef97c787
Daniil_Manuilov <manuilovdaniil@yandex.ru>
2023-01-24 18:18:31 +0100

    add secure folder creation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import requests
 
 
@@ -5,12 +7,14 @@ def download_image(url):
     response = requests.get(url)
     response.raise_for_status()
 
-    with open("hubble.jpeg", 'wb') as f:
+    with open("./images/hubble.jpeg", 'wb') as f:
         f.write(response.content)
 
 
 def main():
     image_url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+
+    Path("./images").mkdir(exist_ok=True)
 
     try:
         download_image(image_url)
