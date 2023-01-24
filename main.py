cbfb308215dfd70405706278fa999fe4363ca612
Daniil_Manuilov <manuilovdaniil@yandex.ru>
2023-01-24 18:25:58 +0100

    add image file path as argument

--- main.py
+++ main.py
@@ -3,21 +3,24 @@ from pathlib import Path
 import requests
 
 
-def download_image(url):
+def download_image(url, file_path):
+    """Функция скачивает изображение и сохраняет его
+    """
     response = requests.get(url)
     response.raise_for_status()
 
-    with open("./images/hubble.jpeg", 'wb') as f:
+    with open(file_path, 'wb') as f:
         f.write(response.content)
 
 
 def main():
     image_url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+    image_file_path = "./images/hubble.jpeg"
 
     Path("./images").mkdir(exist_ok=True)
 
     try:
-        download_image(image_url)
+        download_image(image_url, image_file_path)
     except requests.exceptions.HTTPError:
         print("Что-то пошло не так")
 
