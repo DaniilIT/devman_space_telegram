4ada28b2ac7af6ac816efdb63639f1053c05b773
Daniil_Manuilov <manuilovdaniil@yandex.ru>
2023-01-25 14:17:08 +0100

    create function fetch_nasa_apod

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from pathlib import Path
 from urllib.parse import unquote, urlparse
 
 import requests
+from dotenv import dotenv_values
 
 
 SPACEX_URL = "https://api.spacexdata.com"
-FLIGHT_ID = '5eb87d47ffd86e000604b38a'
+SPACEX_LAUNCH_ID = "5eb87d47ffd86e000604b38a"
+NASA_URL = "https://api.nasa.gov"
+NASA_IMAGES_NUM = 5
 
 
 def download_image(url, file_path):
@@ -19,23 +22,22 @@ def download_image(url, file_path):
         f.write(response.content)
 
 
-def fetch_spacex_last_launch(flight_id):
+def fetch_spacex_last_launch(launch_id):
     """Функция запрашивает через API SpaceX ссылки на изображения и скачивает эти изображения.
     """
-    Path("./images").mkdir(exist_ok=True)
-
     try:
-        response = requests.get(f'{SPACEX_URL}/v5/launches/{flight_id}')
+        response = requests.get(f"{SPACEX_URL}/v5/launches/{launch_id}")
         response.raise_for_status()
     except requests.exceptions.HTTPError:
         print("Что-то пошло не так")
     else:
         for image_number, image_url in enumerate(response.json()['links']['flickr']['original']):
             try:
-                image_file_path = f"./images/spacex{image_number}.jpg"
+                image_file_path = f"./images/spacex_{image_number}.jpg"
                 download_image(image_url, image_file_path)
             except requests.exceptions.HTTPError:
                 print("Что-то пошло не так")
+
 
 def get_file_format(url):
     """Функция возвращает формат файла по адресу.
@@ -45,10 +47,34 @@ def get_file_format(url):
     return file_format
 
 
+def fetch_nasa_apod(token, images_num):
+    """Функция запрашивает через API NASA ссылки на изображения и скачивает эти изображения.
+    """
+    try:
+        payload = {
+            'api_key': token,
+            'count': images_num,
+        }
+        response = requests.get(f"{NASA_URL}/planetary/apod", params=payload)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        print("Что-то пошло не так")
+    else:
+        for image_number, image in enumerate(response.json()):
+            image_url = image['url']
+            try:
+                image_file_path = f"./images/nasa_apod_{image_number}{get_file_format(image_url)}"
+                download_image(image_url, image_file_path)
+            except requests.exceptions.HTTPError:
+                print("Что-то пошло не так")
+
 
 def main():
-    # fetch_spacex_last_launch(FLIGHT_ID)
-    print(get_file_format("https://example.com/txt/hello%20world.txt?v=9#python"))
+    nasa_token = dotenv_values(".env")['NASA_TOKEN']
+    Path("./images").mkdir(exist_ok=True)
+
+    fetch_spacex_last_launch(SPACEX_LAUNCH_ID)
+    fetch_nasa(nasa_token, NASA_IMAGES_NUM)
 
 
 if __name__ == '__main__':
